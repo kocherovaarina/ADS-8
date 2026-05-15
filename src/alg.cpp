01ed5e7305f9d935fc99c919e66659b1f9f1f7ec
kocherovaarina <arinakocerova@gmail.com>
2026-05-15 10:14:50 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,27 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include <string>
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file.is_open()) return;
+    
+    std::string word;
+    while (file >> word) {
+        // Очищаем слово от знаков препинания и приводим к нижнему регистру
+        std::string cleanWord;
+        for (char c : word) {
+            if (std::isalpha(c)) {
+                cleanWord += std::tolower(c);
+            }
+        }
+        if (!cleanWord.empty()) {
+            tree.add(cleanWord);
+        }
+    }
+    file.close();
 }
