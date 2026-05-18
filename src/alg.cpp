ab711a84573afa689088df5b43639e02ef4fa213
kocherovaarina <arinakocerova@gmail.com>
2026-05-18 10:06:29 +0300

    Task 2 is done by Kocherova Arina from 25 IST 4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,32 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return;
+  }
+
+  std::string word;
+  int ch = 0;
+  while ((ch = file.get()) != EOF) {
+    if (ch >= 'A' && ch <= 'Z') {
+      ch = ch - 'A' + 'a';
+    }
+    if (ch >= 'a' && ch <= 'z') {
+      word += static_cast<char>(ch);
+    } else if (!word.empty()) {
+      tree.insert(word);
+      word.clear();
+    }
+  }
+  if (!word.empty()) {
+    tree.insert(word);
+  }
 }
